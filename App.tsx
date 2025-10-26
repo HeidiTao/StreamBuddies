80ab0eead34ed4eabdcfe9e17b085cab46150814
HeidiTao <heidi_tao@outlook.com>
2025-10-26 17:19:36 -0400

    Create README; create simple screen to show TMDB API working

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import APITest from './APITest';
+import APITest from './src/APITest';
 
 export default function App() {
   return (
