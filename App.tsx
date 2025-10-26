dc39a5f9fc61ed2a4bbed3505aea09d3cc913135
HeidiTao <heidi_tao@outlook.com>
2025-10-26 14:41:02 -0400

    Initial app setup

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import APITest from './APITest';
+
+export default function App() {
+  return (
+    <APITest />
+  );
+}
+
